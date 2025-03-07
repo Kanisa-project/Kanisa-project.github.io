3ad8250b7511aa4fb1821ea1b1c7b32f8773aafb
Foreplay241 <project.kanisa@gmail.com>
2025-03-06 22:43:16 -0800

    improved the gallery and wordings.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
 function showContent(contentId) {
     const contents = document.getElementsByClassName('content');
     for (let content of contents) {
@@ -11,18 +14,23 @@ function showContent(contentId) {
 }
 
 function openLightbox(imgElement) {
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImg = document.getElementById('lightbox-img');
     lightboxImg.src = imgElement.src;
     lightbox.style.display = 'flex';
 }
 
 function closeLightbox() {
-    const lightbox = document.getElementById('lightbox');
     lightbox.style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     showContent('home');
 });
-
+document.addEventListener("keydown", function(event) {
+    console.log("Key down: ", event.key);
+    if (event.key === "Escape") {
+        closeLightbox();
+    }
+});
+document.addEventListener("click", function(event) {
+    // closeLightbox();
+});
